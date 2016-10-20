f6b461bc47c28078dd600943903c6b1f23c9f4b7
Mathieu Leplatre <mathieu@mozilla.com>
2016-10-20 14:49:32 +0200

    Python 2.7 support

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,8 +2,8 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 import mock
+from unittest import TestCase
 
-from cornice.tests.support import TestCase
 from cornice_sphinx import rst2html, ServiceDirective
 
 
